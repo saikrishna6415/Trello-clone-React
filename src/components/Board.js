5d70b15a0c882c7602b2aa41b5672d6d9e0235a4
saikrishna <saikrishnakotagiri16@gmail.com>
2020-04-22 18:23:20 +0530

    add function for adding and deleting board

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,14 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Board = props => {
-    // console.log(props)
+  console.log(props)
   return (
-    <div>
-      <Link to={`/board/${props.board.id}`}>
-        <button className='board btn btn-primary'>
-          {props.board.name}
-        </button>
-      </Link>
+    <div className="card">
+      <div className='d-flex justify-content-between'>
+        <Link to={`/board/${props.board.id}`}>
+
+          <button className='board btn btn-primary'>
+            {props.board.name}
+          </button>
+        </Link>
+
+        <button
+          onClick={() => props.deleteBoard(props.board.id)}
+          className='btn-default deleteButtonForBoard' style={{ height: "30px",margin:'10px'}}>
+          X
+            </button>
+      </div>
+
     </div>
   );
 };
--- src/components/Boards.js
+++ src/components/Boards.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import Board from './Board';
+import FormT from './FormT';
 
 
 class Boards extends Component {
-  state = {
-    boards: [],
-    key: 'ffe39d279ee0a46d632ff7b9e7ac02b5',
-    token: '14edac06db12fc2ad32ab72d715ec5d841ee402c02a19e7dc162d6c265a1da6d'
+  constructor() {
+    super()
+    this.state = {
+      boards: [],
+      key: 'ffe39d279ee0a46d632ff7b9e7ac02b5',
+      token: '14edac06db12fc2ad32ab72d715ec5d841ee402c02a19e7dc162d6c265a1da6d',
+      boardName: '',
+      newBoardbutton: true,
+      closeAddForm: false
+    }
+  }
+  // state = {
+  //   boards: [],
+  //   key: 'ffe39d279ee0a46d632ff7b9e7ac02b5',
+  //   token: '14edac06db12fc2ad32ab72d715ec5d841ee402c02a19e7dc162d6c265a1da6d',
+  //   boardName: '',
+  //   newBoardbutton: true,
+  //   closeAddForm: false
+  // };
+
+  newBoardbutton = () => {
+    this.setState(prevState => ({
+      newBoardbutton: !prevState.newBoardbutton,
+      closeAddForm: !prevState.closeAddForm
+    }));
+  };
+  closeAddForm = () => {
+    this.setState(prevState => ({
+      newBoardbutton: !prevState.newBoardbutton,
+      closeAddForm: !prevState.closeAddForm
+    }));
+  }
+  inputState = event => {
+    this.setState({
+      boardName: event.target.value
+    });
   };
   componentDidMount() {
     fetch(`https://api.trello.com/1/members/kotagirisaikrishna1/boards?&key=${this.state.key}&token=${this.state.token}`, {
@@ -22,13 +55,61 @@ class Boards extends Component {
           });
       }).catch(err => console.log(err))
   }
+  addNewBoard = () => {
+    fetch(`https://api.trello.com/1/boards/?name=${this.state.boardName}&key=${this.state.key}&token=${this.state.token}`, {
+      method: 'POST'
+    })
+      .then(data => {
+        data.json()
+          .then(data => {
+            console.log(data)
+            this.setState({
+              boards: this.state.boards.concat([data]),
+              boardName: ''
+            });
+          });
+      }).catch(err => console.log(err))
+  };
+  deleteBoard = id => {
+    console.log(id)
+    console.log(this.state.key)
+    fetch(
+      `https://api.trello.com/1/boards/${id}?key=${this.state.key}&token=${this.state.token}`,
+      {
+        method: 'DELETE'
+      }
+    ).then(() => {
+      this.setState({ boards: this.state.boards.filter(board => board.id !== id) });
+    }).catch(err => console.log(err))
+  };
   render() {
+    var newBoardbutton = this.state.newBoardbutton ? 'block' : 'none';
+    var closeAddForm = this.state.closeAddForm ? 'block' : 'none'
     var allBoards = this.state.boards.map(board => {
-      return <Board key={board.id} board={board} />;
+      return <Board key={board.id}
+        board={board}
+        deleteBoard={this.deleteBoard} />;
     });
     return (
-      <div className="d-flex justfy-content-between">
+      <div className="boards d-flex flex-column">
         {allBoards}
+        <div className="card addList">
+          <button className="addListButton btn btn-primary"
+            onClick={this.newBoardbutton}
+            style={{ display: newBoardbutton }}>
+            Add Board
+                        </button>
+          <FormT
+            style={{ display: closeAddForm }}
+            closeAddForm={this.closeAddForm}
+            inputState={this.inputState}
+            input={this.state.boardName}
+            add={this.addNewBoard}
+            placeholder="Enter Board Name"
+            button="Add Board"
+            width='22rem'
+          />
+        </div>
       </div>
     );
   }
